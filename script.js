668cd97a2540382d2979ab3c6e3fbcc99b80242b
Clement <clementho999@gmail.com>
2023-11-11 00:57:22 +0800

    Can set different speeds with data-speed, both yellow and white cloud works, the final set of white cloud rises up slower than previous sets

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 
 window.addEventListener("scroll", function setAnimation(event) {
-    const white_clouds = document.querySelector(".white_clouds");
-    const speed = white_clouds.dataset.speed;
-    white_clouds.style.transform = "translateY(" + window.scrollY * speed + "px)";
+    // const white_clouds = document.querySelector(".white_clouds");
+    // const speed = white_clouds.dataset.speed;
+    // white_clouds.style.transform = "translateY(" + window.scrollY * speed + "px)";
 
-    
+    // White clouds
+    const white_clouds = document.querySelectorAll(".white_clouds");
+    white_clouds.forEach((element) => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateY(${window.scrollY * speed}px)`;
+    })   
+
+    // Yellow clouds
+    const yellow_clouds = document.querySelectorAll(".yellow_clouds");
+    const yellow_speed = yellow_clouds[0].dataset.speed;
+
+    yellow_clouds.forEach(function (yellow_cloud) {
+        yellow_cloud.style.transform = "translateY(" + window.scrollY * yellow_speed + "px)";
+    });
 });
